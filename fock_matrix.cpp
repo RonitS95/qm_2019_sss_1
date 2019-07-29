ddbe6e14f91947eacb97c12d9443234cdbbbf732
Alessandro Caruso <acaruso@ucsd.edu>
2019-07-28 23:21:49 -0500

    Fock matrix C++ code cleaned

--- fock_matrix.cpp
+++ fock_matrix.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <vector>
 #include <Eigen/Dense>
-//#include <math.h>
 #include <algorithm>
 
 using std::cout; using std::endl; using std::vector; using std::string;
@@ -10,7 +9,7 @@ typedef Eigen::MatrixXd matrix;
 
 int atom(int ao_index, int orbitals_per_atom)
 {
-    return ao_index / orbitals_per_atom; //floor
+    return ao_index / orbitals_per_atom;
 }
 
 int ao_index(int atom_p, string orb_p, vector<string> orbitals, int orbitals_per_atom)
@@ -115,46 +114,6 @@ matrix calculate_fock_matrix(matrix hamiltonian_matrix, matrix interaction_matri
     return fock_matrix;
 }
 
-/*
-def calculate_fock_matrix_fast(hamiltonian_matrix, interaction_matrix, density_matrix, model_parameters):
-    '''Returns the Fock matrix defined by the input Hamiltonian, interaction, & density matrices.'''
-    ndof = np.size(hamiltonian_matrix,0)
-    fock_matrix = hamiltonian_matrix.copy()
-    # Hartree potential term
-    for p in range(ndof):
-        for orb_q in orbital_types:
-            q = ao_index(atom(p), orb_q) # p & q on same atom
-            for orb_t in orbital_types:
-                t = ao_index(atom(p), orb_t) # p & t on same atom
-                chi_pqt = chi_on_atom(orb(p), orb_q, orb_t, model_parameters)
-                for r in range(ndof):
-                    for orb_s in orbital_types:
-                        s = ao_index(atom(r), orb_s) # r & s on same atom
-                        for orb_u in orbital_types:
-                            u = ao_index(atom(r), orb_u) # r & u on same atom
-                            chi_rsu = chi_on_atom(orb(r), orb_s, orb_u, model_parameters)
-                            fock_matrix[p,q] += 2.0 * chi_pqt * chi_rsu * interaction_matrix[t,u] * density_matrix[r,s]
-    # Fock exchange term
-    for p in range(ndof):
-        for orb_s in orbital_types:
-            s = ao_index(atom(p), orb_s) # p & s on same atom
-            for orb_u in orbital_types:
-                u = ao_index(atom(p), orb_u) # p & u on same atom
-                chi_psu = chi_on_atom(orb(p), orb_s, orb_u, model_parameters)
-                for q in range(ndof):
-                    for orb_r in orbital_types:
-                        r = ao_index(atom(q), orb_r) # q & r on same atom
-                        for orb_t in orbital_types:
-                            t = ao_index(atom(q), orb_t) # q & t on same atom
-                            chi_rqt = chi_on_atom(orb_r, orb(q), orb_t, model_parameters)
-                            fock_matrix[p,q] -= chi_rqt * chi_psu * interaction_matrix[t,u] * density_matrix[r,s]
-    return fock_matrix
-
-fock_matrix1 = calculate_fock_matrix(hamiltonian_matrix, interaction_matrix, density_matrix, chi_tensor)
-fock_matrix2 = calculate_fock_matrix_fast(hamiltonian_matrix, interaction_matrix, density_matrix, model_parameters)
-print('difference between fock matrix implementations =', np.linalg.norm(fock_matrix1 - fock_matrix2))
-*/
-
 int main()
 {
     Eigen::MatrixXd ham_test(8,8);
@@ -204,9 +163,6 @@ int main()
     int orbitals_per_atom = orbitals.size();
     double dipole(2.781629275106456);
 
-    //cout << ham_test << endl;
-
-    //cout << floor(2.0 / 3) << " " << floor(2.3) << endl;
     string test_str("px");
 
     int test_atom = atom(2, orbitals_per_atom);
@@ -220,9 +176,6 @@ int main()
      matrix fock_matrix2 = calculate_fock_matrix(ham_test, int_test, dens_test, orbitals, orbitals_per_atom, dipole);
 
      cout << fock_matrix2 << endl;
-
-//    int ndof = ham_test.row(0).size();
-//    cout << ndof << endl;
 
     return 0;
 }
