5faa4a8c80024eb3f9eda2315f8fbdc352bf2b0d
Ronit Sarangi <ronits.chem95@gmail.com>
2019-07-30 22:42:07 -0500

    Update test_qm_project_python_testing.py

--- qm_project_python_testing/tests/test_qm_project_python_testing.py
+++ qm_project_python_testing/tests/test_qm_project_python_testing.py
@@ -3,7 +3,7 @@ Unit and regression test for the qm_project_python_testing package.
 """
 
 # Import package, test suite, and other packages as needed
-import qm_project_python_testing
+import qm_project_python_testing as qm
 import pytest
 import sys
 
